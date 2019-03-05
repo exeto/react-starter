e819ec925272f27c47a78623e2d2fbabafa6cb44
Timofey Dergachev <t@exeto.me>
2019-03-05 23:44:44 +0300

    Remove hack for saving store after hot reload

--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -12,11 +12,6 @@ const composeEnhancers =
     : compose;
 
 export default () => {
-  // https://github.com/parcel-bundler/parcel/issues/314
-  if (window.__STORE__) {
-    return window.__STORE__;
-  }
-
   const { reducer, middleware, enhancer, initialDispatch } = connectRoutes(
     routes,
     { initialDispatch: false },
@@ -36,8 +31,6 @@ export default () => {
   initialDispatch();
 
   if (module.hot) {
-    window.__STORE__ = store;
-
     module.hot.accept('./entities/reducer', () => {
       const newRootReducer = combineReducers({
         entities: require('./entities/reducer').default,
